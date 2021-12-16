ab1367cd34cc0b7b6a1325c52debdc7fb0b9e48b
MithunCh <mithun.howlader222@gmail.com>
2021-12-17 00:31:15 +0600

    save BubbleSortAndAlgorithms

---
+++ DatastructuresAndAlgorithms/BubbleSortAndAlgorithms/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] bubbleArray = new int[] { 10, 5, 6, 12, 3 };
+        var Count = 0;
+        for (int i = 0; i < bubbleArray.Length; i++)
+        {
+            for (int j = 0; j < bubbleArray.Length -i- 1; j++)
+            {
+                Count++;
+                if (bubbleArray[j] > bubbleArray[j + 1])
+                {
+                    var temp = bubbleArray[j];
+                    bubbleArray[j] = bubbleArray[j + 1];
+                    bubbleArray[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < bubbleArray.Length; i++)
+        {
+            Console.Write($" {bubbleArray[i]}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Total loop cycle count {Count} times.");
+    }
+}
