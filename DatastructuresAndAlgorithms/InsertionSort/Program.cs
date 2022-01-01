6d524cb94081b728abb0881e0fd58d2c14a1aabe
MithunCh <mithun.howlader222@gmail.com>
2022-01-02 01:14:02 +0600

    commit insertion short algorithms

---
+++ DatastructuresAndAlgorithms/InsertionSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] insertionArray = new int[] { 10, 5, 6, 12, 3 };
+
+            for (int i = 1; i < insertionArray.Length; i++)
+            {
+                var item = insertionArray[i];
+                var j = i - 1;
+
+                while(j >= 0 && insertionArray[j] > item)
+                {
+                    insertionArray[j + 1] = insertionArray[j];
+                    j = j - 1;
+                }
+
+                insertionArray[j+1] = item;
+            }
+
+            for (int i = 0; i < insertionArray.Length; i++)
+            {
+                Console.Write($" {insertionArray[i]}");
+            }
+        }
+    }
+}
