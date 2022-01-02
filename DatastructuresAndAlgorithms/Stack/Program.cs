01342cf770eb371c83e52a6b37a8e13744b80953
MithunCh <mithun.howlader222@gmail.com>
2022-01-03 00:21:37 +0600

    push in the stack structure

---
+++ DatastructuresAndAlgorithms/Stack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackD = new StackData(5);
+            stackD.push(10);
+            stackD.push(11);
+            stackD.push(12);
+            stackD.push(13);
+            stackD.push(14);
+
+            stackD.push(15);
+
+            stackD.pop();
+            stackD.pop();
+            stackD.pop();
+            stackD.pop();
+            stackD.pop();
+
+            stackD.pop();
+
+            stackD.push(10);
+            stackD.push(11);
+            stackD.push(12);
+            stackD.push(13);
+            stackD.push(14);
+        }
+
+        
+    }
+}
---
+++ DatastructuresAndAlgorithms/Stack/StackData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackData
+    {
+
+        int max;
+        int item;
+        int top = 0;
+        int[] stackArray;
+
+        public StackData(int size)
+        {
+            stackArray = new int[size];
+            max = size;
+        }
+
+        public void push(int item)
+        {
+            if(top < max)
+            {
+                stackArray[top] = item;
+                Console.WriteLine($"Push the item {stackArray[top]}");
+                top++;
+            }
+            else
+            {
+                Console.WriteLine("Stack is full");
+            }
+        }
+
+        public int pop()
+        {
+            if(top == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return 0;
+            }
+            else
+            {
+                top--;
+                item = stackArray[top];
+                stackArray[top] = 0;
+                Console.WriteLine($"Pop the item {item}");
+                return item;
+            }
+        }
+    }
+}
